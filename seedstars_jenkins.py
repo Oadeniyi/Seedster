7988dbe415a670bb840cd2b495af21f886fd66d3
segzoid <39262149+segzoid@users.noreply.github.com>
2018-05-16 15:25:23 +0100

    Add files via upload

--- seedstars_jenkins.py
+++ seedstars_jenkins.py
@@ -1,35 +1,65 @@
 import jenkins
 import sqlite3
 import sys
-from datetime import datetime
+from datetime import datetime,date
 
-url = sys.argv[1]
-usr = sys.argv[2]
-pwd = sys.argv[3]
+
+#url = sys.argv[1]
+url = input('Please enter the URL here:')
+#usr = sys.argv[2]
+usr = input('Username:')
+#pwd = sys.argv[3]
+pwd = input('Password:')
 
 def populate_target(jenkinsurl, user, pss):
+	server = jenkins.Jenkins(jenkinsurl, username=user, password=pss)
 	try:
-		server = jenkins.Jenkins(jenkinsurl, username=user, password=pss)
 		conn = sqlite3.connect('seedstars.db')
 		c = conn.cursor()
-		sql_cmd1 = '''create table Jobs_status_time (job text, status text, timestamp time)'''
+		sql_cmd1 = '''create table Jobs_status_time (job text, status text, timestamp text)'''
 		c.execute(sql_cmd1)
+		jobs = server.get_jobs()
+		for job in jobs:
+			if "result" in job.keys():
+				name = job['name']
+				status = jobs['result']
+				time = datetime.strftime(datetime.now(),"%Y%m%d %H:%M:%S") 
+				print(name,status,time)
+				c.execute("""insert into Jobs_status_time('job', 'status', 'timestamp') values(?,?,?)""",(name, status, time))
+				conn.commit()
+			else:
+				name = job['name']
+				time = datetime.strftime(datetime.now(),"%Y%m%d %H:%M:%S") 
+				print(name,time)
+				c.execute("""insert into Jobs_status_time('job', 'timestamp') values(?,?)""",(name, time))
+				conn.commit()
+				print('Job has no status')
 	except sqlite3.Error:
-		print('Table already exixts...')
-	jobs = server.get_jobs()
-	for job in jobs:
-		if "result" in job.keys():
-			name = job['name']
-			status = jobs['result']
-			time = datetime.now()
-			print(name,status,time)
-			sql_cmd = '''insert into Job_status(job, status) /nvalues({},{})'''.format(name,status,time)
-			c.execute(sql_cmd)
-			conn.commit()
-		else:
-			name = job['name']
-			time = datetime.now()
-			print(name,time)
-			print('Job has no status')
+		print('table already exist..')
+		conn = sqlite3.connect('seedstars.db')
+		c = conn.cursor()
+		jobs = server.get_jobs()
+		for job in jobs:
+			if "result" in job.keys():
+				name = job['name']
+				status = jobs['result']
+				time = datetime.strftime(datetime.now(),"%Y%m%d %H:%M:%S") 
+				print(name,status,time)
+				c.execute("""insert into Jobs_status_time('job', 'status', 'timestamp') values(?,?,?)""",(name, status, time))
+				conn.commit()
+			else:
+				name = job['name']
+				time = datetime.strftime(datetime.now(),"%Y%m%d %H:%M:%S") 
+				print(name,time)
+				c.execute("""insert into Jobs_status_time('job', 'timestamp') values(?,?)""",(name, time))
+				conn.commit()
+				print('Job has no status')
+	
 
 populate_target(url, usr, pwd)
+# connt = sqlite3.connect('seedstars.db')
+# c = connt.cursor()
+# c.execute('select * from Jobs_status_time')
+# rows = c.fetchall()
+# for i in rows:
+# 	print(i)
